34f984cc62d3b7de56748a8b5b640d41543e1773
Niranjana Padmanabhan <niranjanapadmanabhan1@gmail.com>
2025-12-26 18:40:51 -0500

    commit

--- test_orangeHRM login.py
+++ test_orangeHRM login.py
@@ -1,11 +1,10 @@
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 import time
 
-def test_orangeHRM_login():
-    driver=webdriver.Chrome()
-    driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
-    driver.maximize_window()
-    time.sleep(2)
+
+driver=webdriver.Chrome()
+driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+driver.maximize_window()
+time.sleep(2)
